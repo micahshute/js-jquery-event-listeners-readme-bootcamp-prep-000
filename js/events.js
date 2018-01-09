e7b9335e4e35dff1675c59d8073c91c3051233f6
Micah Shute <micah.shute@gmail.com>
2018-01-09 02:22:52 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,4 +1,30 @@
-//define functions here
+function getIt(){
+  $("p").on("click", function(){
+    alert("Hey!")
+  })
+} 
+
+function frameIt(){
+  $('img').on('load', function(){
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt(){
+  $('#typing').on('keydown', function(key){
+    if(key.which === 71){
+      alert('g key pressed');
+    }
+  })
+}
+
+function submitIt(){
+  $('form').on('submit', function(){
+    alert('Your form is going to be submitted now.');
+  })
+}
+
+getIt();frameIt();pressIt();submitIt();
 
 $(document).ready(function(){
 
